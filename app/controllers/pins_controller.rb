463d93ee53a6766e207885fb0f767101c58d555e
Gilbert Sanchez <gillsan1@gmail.com>
2013-05-05 16:45:08 -0700

    Add Pins Scaffold

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -57,7 +57,7 @@ class PinsController < ApplicationController
   # PUT /pins/1
   # PUT /pins/1.json
   def update
-    @pin = Pin.find(params[:id])
+    @pin = current_user.pins.find(params[:id])
 
     respond_to do |format|
       if @pin.update_attributes(params[:pin])
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,5 +7,6 @@ class Pin < ActiveRecord::Base
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
   belongs_to :user
+  validates :user_id, presence: true
   has_attached_file :image, styles: { medium: "320x240>"}
 end
